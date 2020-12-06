ecb9fabee9031275053654a043adaf8c0ebee468
VishnuKarikkal <sajanvishnu0@gmail.com>
2020-12-07 00:50:28 +0530

    search + sort functionalities added

--- ngApp/src/app/product-list/product-list.component.ts
+++ ngApp/src/app/product-list/product-list.component.ts
@@ -12,6 +12,8 @@ export class ProductListComponent implements OnInit {
   constructor(private productsService:ProductsService) { }
 title:string="Product List";
 products=<any>[];
+results;
+result=false;
 available = false;
   ngOnInit(): void {
     this.productsService.getProducts().subscribe((data)=>
@@ -29,4 +31,38 @@ available = false;
                                                   });
   }
 
+//search by product name
+  search()
+  {
+   let search_term = document.getElementById('search-input')['value'].trim();
+   if(search_term)
+   {
+    const search_Results= this.products.filter((arr)=>
+        {
+            return arr.name == search_term;
+        })
+        if(search_Results.length > 0)
+        {
+          this.result = true;
+          this.results = search_Results;
+        }
+        else
+        {
+          this.results=null;
+          this.result=false;
+        }
+   }
+  }
+  //sort by name : ascending order
+  sortProducts()
+  {
+    this.products.sort((a,b)=>
+    {
+      let aName = a.name;
+      let bName = b.name;
+      if(aName < bName) return -1;
+      if(aName > bName) return 1;
+      return 0;
+    })
+  }
 }
